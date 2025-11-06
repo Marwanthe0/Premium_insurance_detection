0eba4ec8cf060d4876310eef5125aaa88724ae5f
Marwanthe0 <marwanthe0@gmail.com>
2025-11-07 01:58:08 +0600

    Frontend creation with Streamlit, importing modules, and fetching the api key url

---
+++ frontend.py
@@ -0,0 +1,9 @@
+# Importing necessary libraries
+import streamlit as st
+import requests
+
+# Fetching the api key
+API_URL = "http://127.0.0.1:8000/predict"
+
+# Title of the project
+st.title("Insurance premium category Predictor using Machine learning model")
